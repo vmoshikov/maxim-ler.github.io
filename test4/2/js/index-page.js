564581aea51648376001cca42d1b87f42d8ed173
vasily-d <den771981@mail.ru>
2018-03-04 15:00:14 +0300

    add

--- test4/2/js/index-page.js
+++ test4/2/js/index-page.js
@@ -1,13 +1,15 @@
 $(function() {
 	// slick slider
 	$('.slider1').slick({
-		slidesToShow: 2,
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false,
 		dots: true,
 		autoplay: true,
 		autoplaySpeed: 2000
 	});
+
+
 
 	// popups
 	// ставим на кнопку которая вызывает данный popup
@@ -31,7 +33,63 @@ $(function() {
 	  });
 	});
 
+
+
+	// magnific-popup
+	$('.image-link').magnificPopup({type:'image'});
+	$('.popup-link1').magnificPopup({
+	  type: 'image'
+	  // other options
+	});
 	
 
 
+	// email - new with verify
+	$('.main-form').submit(function(event) {
+		event.preventDefault();
+		
+		// проверка на стороне клиента формы через js что форма не пустая
+		var name = $('.name1').val();
+		var phone = $('.tel1').val();
+		// console.log(name);
+		// console.log(phone);
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			
+			//alert("Заявка отправлена!");
+			window.location.href = "../mailer/thanks.html";
+			
+			$("form").trigger("reset");
+			
+			$('.popup').slideUp();
+			$('.popup-overlay').css('display', 'none');
+		});
+
+		// if (name !== '' & phone !== '') {
+		// 	$.ajax({
+		// 		type: "POST",
+		// 		url: "mailer/smart.php",
+		// 		data: $(this).serialize()
+		// 	}).done(function() {
+		// 		$(this).find("input").val("");
+				
+		// 		//alert("Заявка отправлена!");
+		// 		window.location.href = "../mailer/thanks.html";
+				
+		// 		$("form").trigger("reset");
+				
+		// 		$('.popup').slideUp();
+		// 		$('.popup-overlay').css('display', 'none');
+		// 	});
+		// } else {
+		// 	alert('Данные не заполнены!');
+		// }
+		
+		return false;
+	});
 });
