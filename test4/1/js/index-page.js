18108b2b9f3054939237ed84b19ef1ce0aff8011
vasily-d <den771981@mail.ru>
2018-03-19 16:42:54 +0300

    fix

--- test4/1/js/index-page.js
+++ test4/1/js/index-page.js
@@ -29,7 +29,55 @@ $(function() {
 	  });
 	});
 
+	// email - new with verify
+	$('.main-form').submit(function(event) {
+		event.preventDefault();
+		
+		// проверка на стороне клиента формы через js что форма не пустая
+		var name = $('#name1').val();
+		var phone = $('#tel1').val();
+		// console.log(name);
+		// console.log(phone);
 
+		if (name !== '' & phone !== '') {
+			$.ajax({
+				type: "POST",
+				url: "mailer/smart.php",
+				data: $(this).serialize()
+			}).done(function() {
+				$(this).find("input").val("");
+				
+				//alert("Заявка отправлена!");
+				window.location.href = "../mailer/thanks.html";
+				
+				$("form").trigger("reset");
+				
+				$('.popup').slideUp();
+				$('.popup-overlay').css('display', 'none');
+			});
+		} else {
+			alert('Данные не заполнены!');
+		}
+		return false;
+	});
+
+	// jQuery smooth scrolling
+	$('.go_to1').click( function() { // ловим клик по ссылке с классом go_to1
+		var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+	  
+		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 900); // анимируем скроолинг к элементу scroll_el
+		}
+
+		return false; // выключаем стандартное действие
+	});
+
+	// magnific-popup
+	$('.image-link').magnificPopup({type:'image'});
+	$('.popup-link1').magnificPopup({
+	  type: 'image'
+	  // other options
+	});
 
 
 });
